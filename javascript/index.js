085d53bf474f59f4d2e7a90ee4618649e380be8e
baroned1707 <61402493+baroned1707@users.noreply.github.com>
2020-06-19 18:32:39 +0700

    foodstore

--- javascript/index.js
+++ javascript/index.js
@@ -159,26 +159,23 @@ $(document).ready(()=>{
     }  
 })
 
-<<<<<<< HEAD
-$(document).ready(() => {
-    let html;
+// $(document).ready(() => {
+//     let html;
+// });
+// $(document).ready(()=>{
+//     let len = list_all.length;
+//     let html;
+//     if (len%9 !== 0){
+//         let count = (len/9) + 1;
+//     }
+//     else let count = len/9;
 
-=======
-$(document).ready(()=>{
-    let len = list_all.length;
-    let html;
-    if (len%9 !== 0){
-        let count = (len/9) + 1;
-    }
-    else let count = len/9;
+//     for (let n = 0; n < count; n++) {
+//         html += `<input type="button" value="1" id="page_1">`
+//     }
 
-    for (let n = 0; n < count; n++) {
-        html += `<input type="button" value="1" id="page_1">`
-    }
-
-    $("#page").append(html)
->>>>>>> ad135accc8868e1843517ea365bbb7b7c6edff8b
-})
+//     $("#page").append(html)
+// })
 
 $(document).ready(function () {
     $("#page_1").click(function () { 
