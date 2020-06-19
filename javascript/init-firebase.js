365b964f71c08c59a1a7a8ced3a54da089abdfb9
baroned1707 <61402493+baroned1707@users.noreply.github.com>
2020-06-19 13:22:57 +0700

    foodstore

---
+++ javascript/init-firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAajAhRiMBVudXjLqKGaU4dUddd95-N_S0",
+    authDomain: "foodstore-83082.firebaseapp.com",
+    databaseURL: "https://foodstore-83082.firebaseio.com",
+    projectId: "foodstore-83082",
+    storageBucket: "foodstore-83082.appspot.com",
+    messagingSenderId: "1068541464296",
+    appId: "1:1068541464296:web:6db72fbd23cee9f7ea835d",
+    measurementId: "G-6KXCZVV5RC"
+  };
+export const App = firebase.initializeApp(firebaseConfig);
