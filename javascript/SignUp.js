a26fdb16b3210b4fef3a2f88c91ccb4b6c61838c
baroned1707 <61402493+baroned1707@users.noreply.github.com>
2020-07-09 09:48:06 +0700

    foodstore

--- javascript/SignUp.js
+++ javascript/SignUp.js
@@ -13,6 +13,21 @@ $(document).ready(()=>{
         console.log(pass);
         console.log(address);
         //Dieu kien
+        if(!(name.length>0)){
+            alert("Ten khong dung");
+        }
+        if(!(sdt.length>0&&RegExp('((09|03|07|08|05)+([0-9]{8})\b)').test(sdt))){
+            alert("SDT khong dung");
+        }
+        if(!(mail.length>0&&RegExp('^[a-z][a-z0-9_\.]{5,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}$').test(mail))){
+            alert("Email khong dung");
+        }
+        if(!(pass.length>0)){
+            alert('Pass khong dung');
+        }
+        if(!(address.length>0)){
+            alert('Dia chi khong dung');
+        }
 
         //Login va khoi tao database cho tai khoang
         firebase.auth().createUserWithEmailAndPassword(mail, pass)
@@ -33,7 +48,7 @@ $(document).ready(()=>{
             var errorMessage = error.message;
             // ...
             console.log(error.message);
+            alert('Dang ky khong thanh cong ! Vui long thu lai !');
         })
-        
     })
 })
